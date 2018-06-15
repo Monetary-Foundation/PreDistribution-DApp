9ab15e5819b15096c40ace50e13532ed5065f1e6
Paul Laux <lauxpaul@protonmail.com>
2018-06-15 10:21:46 +0300

    spacing fix

--- app/components/Web3Status/index.js
+++ app/components/Web3Status/index.js
@@ -57,7 +57,7 @@ function Web3Status(props) {
       Token Contract: <br />
       <AddressLink address={tokenAddress} networkId={networkId} />
       <br /> <br />
-      Web3 Provider: <br /> {provider || 'no web3 provider specified'} <br /><br />
+      Web3 Provider: {provider || 'no web3 provider specified'} <br /><br />
       <b><a target="_blank" rel="noopener" href="https://monetarycoin.org/distribution">Distribution Details</a></b><br />
       <a target="_blank" rel="noopener" href="https://monetarycoin.org">MonetaryCoin Homepage</a><br />
       <a target="_blank" rel="noopener" href="https://github.com/Monetary-Foundation/MonetaryCoin">MonetaryCoin source code</a><br /><br />

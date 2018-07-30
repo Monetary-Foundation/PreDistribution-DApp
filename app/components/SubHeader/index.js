793e72020fbf2b71beff71805d197e069977331b
Paul Laux <lauxpaul@protonmail.com>
2018-07-30 14:28:36 +0300

    added exchange list

--- app/components/SubHeader/index.js
+++ app/components/SubHeader/index.js
@@ -8,7 +8,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Row, Col } from 'antd';
-import { exchanges } from 'utils/constants';
+// import { exchanges } from 'utils/constants';
 
 const RowS = styled(Row)`
   div{
@@ -19,16 +19,21 @@ const RowS = styled(Row)`
   }
 `;
 
+const url = 'https://monetarycoin.org/monetarycoin_exchange_list';
+
 function SubHeader(props) {
   const { tokenSymbol } = props;
   const token = tokenSymbol || 'MERO';
-  const { name, url } = exchanges[token][0];
+  // const { name, url } = exchanges[token][0];
   return (
     <RowS type="flex" >
       <Col xs={{ span: 20, offset: 1 }}>
         <span>{"Don't"} like to wait?
-          <a target="_blank" rel="noopener" href={url}>
+          {/* <a target="_blank" rel="noopener" href={url}>
             {` ${tokenSymbol}`} is traded on {name}
+          </a> */}
+          <a target="_blank" rel="noopener" href={url}>
+            {` ${token}`} Exchange List
           </a>
         </span>
       </Col>
